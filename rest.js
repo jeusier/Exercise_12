2dde52e61b0cf24048a2edfe5f71fa6af9f37f42
Jason Piros <jahsan87@gmail.com>
2014-02-11 21:33:54 -1000

    added comments for easier code reading

--- rest.js
+++ rest.js
@@ -1,8 +1,8 @@
 var http = require('http');
 var url = require('url');
 var qs = require('querystring');
-//var id_length = 0;
 
+//Link class for links
 function Link(name, url) {
 	this.name = name;
 	this.url = url;
@@ -10,109 +10,130 @@ function Link(name, url) {
 
 }
 
+//array to hold all Link instances
 var links = [];
 
+//prints out all links currently in the array
 function findAllLinks(req, res) {
-	console.log(links);
+	//console.log(links);
 	var temp = "";
+	//loop through the array and store into a temporary string
 	for (var i = 0; i < links.length; i++){
 		temp += "<a id='"+links[i].id+"' href='"+links[i].url+"'>"+links[i].name+"</a>\n";
 
 	}
 	res.writeHead(200, {'Content-Type': 'text/plain'});
+	//displays all links to user
 	res.end(temp + '\n');
 
 }
 
+//prints out the link with the user-specified id
 function findLinkById(req, res, link_id) {
+	var id_link = "";
+	//loop through array to find the link with the specified id and store into variable as a string
 	for (var i = 0; i < links.length; i++){
 		if (links[i].id == link_id){
-			var id_link = "<a id='"+links[i].id+"' href='"+links[i].url+"'>"+links[i].name+"</a>\n";
+			id_link = "<a id='"+links[i].id+"' href='"+links[i].url+"'>"+links[i].name+"</a>\n";
 		}
 	}
 	res.writeHead(200, {'Content-Type': 'text/plain'});
+	//display the link with specified id to user
 	res.end(id_link + '\n');
 
 }
 
+//create link and add to link array
 function createLink(req, res, link_id) {
 	var body = '';
+	//stores the data string into the body variable
 	req.on('data', function(DATA) { // on is an event listener
 		body += DATA;
 	});
 	req.on('end', function() {
-		//console.log(body);
+		//parses body into an object
 		var params = qs.parse(body);
-		//console.log(params);
+		//create new Link instance using the params object
 		var newLink = new Link(params.name, params.url);
-		//console.log(newLink);
+		//add new Link instance into links array
 		links.push(newLink);
 		console.log(links);
+		//display to user that a link was added
 		res.end('Added Link\n');
 	});
 }
 
+//update a link with an already used id
 function updateLink(req, res, link_id) {
 		var body = '';
+	//stores the data string into the body variable
 	req.on('data', function(DATA) { // on is an event listener
 		body += DATA;
+		console.log(body);
 	});
 	req.on('end', function() {
-		//console.log(body);
+		//parses body into an object
 		var params = qs.parse(body);
-		//console.log(params);
-		//var newLink = new Link(params.name, params.url);
+		//loop through links array to find the link that has the user specified id
 		for (var i = 0; i < links.length; i++){
+			//if link with specified id is found, replace current values 
 			if (links[i].id == link_id){
 					links[i].id = link_id;
 					links[i].name = params.name;
 					links[i].url = params.url;
 			}
 		}
-
-
-		//console.log(newLink);
 		console.log(links);
+		//display to user that a link was updated
 		res.end('Updated Link\n');
 	});
 }
 
+//delete link with specified id from the links array
 function deleteLink(req, res, link_id) {
+	//loop through links array to find the link with the specified id
 	for (var i = 0; i < links.length; i++){
+		//if link with specified id is found, remove from array
 		if (links[i].id == link_id){
 			links.splice(i, 1);
 		}
 	}
 	console.log(links);
 	res.writeHead(200, {'Content-Type': 'text/plain'});
+	//display to user that link was deleted
 	res.end('Deleted Link\n');
-
 }
 
-
+//will route depending on the method
 function allLinks(req, res, link_id) {
 	res.writeHead(200, {
 		'Content-Type': 'text/plain'
 	});
+	//if trying to find all links or a link by id
 	if (req.method == 'GET') {
+		//if no id specified, get all links
 		if (link_id === undefined){
 			findAllLinks(req, res, link_id);
+		//if id is specified, get specified link
 		} else {
 			console.log(link_id);
 			findLinkById(req, res, link_id);
 		}
+	//if creating new link
 	} else if (req.method == 'POST') {
 		createLink(req, res, link_id);
+	//if updating link
 	} else if (req.method == 'PUT') {
 		updateLink(req, res, link_id);
+	//if deleting link
 	} else if (req.method == 'DELETE') {
 		deleteLink(req, res, link_id);
 	} else {
-		res.end('Invalid POST\n');
+		res.end('Invalid method\n');
 	}
 }
 
-
+//displays page not found if user types in incorrectly
 function show404(req, res) {
 	res.writeHead(404, {
 		'Content-Type': 'text/plain'
@@ -120,13 +141,17 @@ function show404(req, res) {
 	res.end('Page Not Found! ><\n');
 }
 
+
 http.createServer(function(req, res) {
 	var url_parts = url.parse(req.url);
+	//splits the url at the / into an array
 	var split_url = url_parts.pathname.split("/");
+	//stores the /links/
 	var resource = "/"+split_url[1]+"/";
+	//stores the id
 	var link_id = split_url[2];
 	console.log(link_id);
-
+	//based on the url, do something
 	switch (resource) {
 		case '/links/':
 			allLinks(req, res, link_id);
