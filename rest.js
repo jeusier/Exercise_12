7915bb607a19e7652a1a323f92fb675c032cdf00
Jason Piros <jahsan87@gmail.com>
2014-02-11 20:21:48 -1000

    completed DELETE /links/:id

--- rest.js
+++ rest.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var url = require('url');
 var qs = require('querystring');
+//var id_length = 0;
 
 function Link(name, url) {
 	this.name = name;
@@ -12,9 +13,10 @@ function Link(name, url) {
 var links = [];
 
 function findAllLinks(req, res) {
+	console.log(links);
 	var temp = "";
 	for (var i = 0; i < links.length; i++){
-		temp += "<a href='"+links[i].url+"'>"+links[i].name+"</a>\n";
+		temp += "<a id='"+links[i].id+"' href='"+links[i].url+"'>"+links[i].name+"</a>\n";
 
 	}
 	res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -23,25 +25,27 @@ function findAllLinks(req, res) {
 }
 
 function findLinkById(req, res, link_id) {
-	var id_link = "<a href='"+links[link_id].url+"'>"+links[link_id].name+"</a>\n";
+	var id_link = "<a id='"+links[link_id].id+"' href='"+links[link_id].url+"'>"+links[link_id].name+"</a>\n";
 
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end(id_link + '\n');
 
 }
 
-function createLink(req, res) {
+function createLink(req, res, link_id) {
+	console.log(links.indexOf(link_id));
 	var body = '';
 	req.on('data', function(DATA) { // on is an event listener
 		body += DATA;
 	});
 	req.on('end', function() {
-		console.log(body);
+		//console.log(body);
 		var params = qs.parse(body);
-		console.log(params);
+		//console.log(params);
 		var newLink = new Link(params.name, params.url);
-		console.log(newLink);
+		//console.log(newLink);
 		links.push(newLink);
+		console.log(links);
 		res.end('Added Link\n');
 	});
 }
@@ -52,87 +56,52 @@ function updateLink(req, res, link_id) {
 		body += DATA;
 	});
 	req.on('end', function() {
-		console.log(body);
+		//console.log(body);
 		var params = qs.parse(body);
-		console.log(params);
+		//console.log(params);
 		var newLink = new Link(params.name, params.url);
-		console.log(newLink);
+		//console.log(newLink);
 		links.push(newLink);
+		console.log(links);
 		res.end('Added Link\n');
 	});
 }
 
 function deleteLink(req, res, link_id) {
-		var body = '';
-	req.on('data', function(DATA) { // on is an event listener
-		body += DATA;
-	});
-	req.on('end', function() {
-		console.log(body);
-		var params = qs.parse(body);
-		console.log(params);
-		var newLink = new Link(params.name, params.url);
-		console.log(newLink);
-		links.push(newLink);
-		res.end('Added Link\n');
-	});
+	//console.log(links.indexOf(link_id));
+	//var linkToDelete = links.indexOf(link_id);
+	//console.log(links.indexOf(link_id));
+	//console.log(links);
+	links.splice(link_id, 1);
+	console.log(links);
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Deleted Link\n');
+
 }
+
 
 function allLinks(req, res, link_id) {
 	res.writeHead(200, {
 		'Content-Type': 'text/plain'
 	});
 	if (req.method == 'GET') {
-		if (link_id == 'undefined'){
-			findAllLinks(req, res);
+		if (link_id === undefined){
+			findAllLinks(req, res, link_id);
 		} else {
+			console.log(link_id);
 			findLinkById(req, res, link_id);
 		}
 	} else if (req.method == 'POST') {
-		createLink(req, res);
+		createLink(req, res, link_id);
 	} else if (req.method == 'PUT') {
-		updateLink(req, res);
+		updateLink(req, res, link_id);
 	} else if (req.method == 'DELETE') {
-		deleteLink(req, res);
+		deleteLink(req, res, link_id);
 	} else {
 		res.end('Invalid POST\n');
 	}
 }
 
-
-
-function linksById(req, res) {
-	res.writeHead(200, {
-		'Content-Type': 'text/plain'
-	});
-	if (req.method == 'GET') {
-		var temp = "";
-		for (var i = 0; i < links.length; i++) {
-			temp += "<a id='" + i + "' href='" + links[i].url + "'>" + links[i].name + "</a>\n";
-		}
-		res.writeHead(200, {
-			'Content-Type': 'text/plain'
-		});
-		res.end(temp + '\n');
-	}
-
-
-
-	if (req.method == 'POST') {
-		var body = '';
-		req.on('data', function(DATA) { // on is an event listener
-			body += DATA;
-		});
-		req.on('end', function() {
-			var params = qs.parse(body);
-			console.log(params);
-			links.push(params);
-			res.end('Added Link\n');
-		});
-	} else {
-		res.end('Invalid POST\n');
-	}
-}
 
 function show404(req, res) {
 	res.writeHead(404, {
@@ -146,6 +115,7 @@ http.createServer(function(req, res) {
 	var split_url = url_parts.pathname.split("/");
 	var resource = "/"+split_url[1]+"/";
 	var link_id = split_url[2];
+	console.log(link_id);
 
 	switch (resource) {
 		case '/links/':
