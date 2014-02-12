d0cfedf8b23d4c8af032c0b3d46b0defd326926b
Jason Piros <jahsan87@gmail.com>
2014-02-11 14:50:59 -1000

    rendered an array of objects defined myself

--- example.js
+++ example.js
@@ -1,13 +1,33 @@
 var http = require('http');
 var url = require('url');
-function index(req, res){
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello Index\n');
+
+function Link(name, url) {
+	this.name = name;
+	this.url = url;
 }
+
+var links = [
+	new Link("Youtube", "youtube.com"),
+	new Link("Google", "google.com"),
+	new Link("Reddit", "reddit.com")
+	];
+
+function index(req, res){
+	var temp = "";
+	for (var i = 0; i < links.length; i++){
+		temp += "<a href='"+links[i].url+"'>"+links[i].name+"</a>\n";
+
+	}
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end(temp + '\n');
+
+}
+
 function show404(req, res){
 	res.writeHead(404, {'Content-Type': 'text/plain'});
 	res.end('Page Not Found! ><\n');
 }
+
 http.createServer(function (req, res) {
 	var url_parts = url.parse(req.url);
 	switch(url_parts.pathname){
