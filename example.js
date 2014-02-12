fe60e49c1dcf105af71c4c1545a081078117c2c4
Jason Piros <jahsan87@gmail.com>
2014-02-11 14:22:21 -1000

    learned routing

--- example.js
+++ example.js
@@ -1,6 +1,22 @@
 var http = require('http');
-http.createServer(function (req, res) {
+var url = require('url');
+function index(req, res){
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World\n');
+	res.end('Hello Index\n');
+}
+function jason(req, res){
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Hello Jason\n');
+}
+http.createServer(function (req, res) {
+	var url_parts = url.parse(req.url);
+	switch(url_parts.pathname){
+		case '/':
+			index(req, res);
+			break;
+		case '/jason':
+			jason(req, res);
+			break;
+	}
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
